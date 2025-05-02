69478b5cb3cdb8d71243a670d3a86582949cbf2a
Balázs Marcell <KoegLike@proton.me>
2025-05-02 10:32:45 +0300

    removed tmux atach

--- home.nix
+++ home.nix
@@ -13,15 +13,6 @@
     config.allowUnfree = true;
   };
 
-  programs.bash = {
-    enable = true;
-    initExtra = ''
-      if command -v tmux &> /dev/null && [ -z "$TMUX"]; then 
-        tmux attach-session -t main || tmux new-session -s main
-      fi
-    '';
-  };
-
   home.username = "koeg";
   home.homeDirectory = "/home/koeg";
   programs.home-manager.enable = true;
